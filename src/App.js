cec47a4b07803d9bbecdb45991221b3583799f4d
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-06-30 13:12:27 -0300

    Consertando ESlint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState, useContext } from 'react';
-import AppContext from './AppContext';
+import React from 'react';
 import Table from './components/Table';
 import Provider from './Provider';
 
 function App() {
-  
-  // <input type="text" onChange={ (e) => setNamePlanet(e.target.value) } data-testid='name-filter' value={ namePlanet } />
   return (
     <Provider>
-      
       <Table />
     </Provider>
   );
